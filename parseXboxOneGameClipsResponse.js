d66545629ae93cc075c0ca51ff7e49e6f7abb77e
Pete Lancaster <petedoeswebthings@gmail.com>
2014-11-30 17:58:43 +0000

    Added a simplistic way to trigger multiple game clip downloads in a chain. Will return to make the code cleaner at a later date.

--- parseXboxOneGameClipsResponse.js
+++ parseXboxOneGameClipsResponse.js
@@ -1,21 +1,204 @@
 var fs = require('fs');
+var cp = require('child_process');
+var Q = require('q');
+var http = require('http');
+
 var parsedJSON = require(process.argv[2]);
-
-//console.log(parsedJSON[0]);
-
-var outputTitle = parsedJSON[0].titleName.replace(/\W+/g, "") + '-' + parsedJSON[0].dateRecorded.replace(/\W+/g, "-");
-//console.log(outputTitle);
-//console.log(parsedJSON[0].gameClipUris[0].uri);
-
-var uniqueOutputTitle;
+var fileDir = process.argv[3];
+var gamertag = process.argv[4];
+var downloadArray = [];
 
 for (var i = parsedJSON.length - 1; i >= 0; i--) {
 
 	for (var p = parsedJSON[i].gameClipUris.length - 1; p >= 0; p--) {
-		if(parsedJSON[i].gameClipUris[p].uriType === 'Download'){
-			uniqueOutputTitle = parsedJSON[i].titleName.replace(/\W+/g, "") + '-' + parsedJSON[i].dateRecorded.replace(/\W+/g, "-");
-			console.log(uniqueOutputTitle);
-			console.log(parsedJSON[i].gameClipUris[p].uri);
+		var pathToFile = fileDir + '/' + gamertag + '-' + parsedJSON[i].titleName.replace(/\W+/g, "") + '-' + parsedJSON[i].dateRecorded.replace(/\W+/g, "-") + '.MP4';
+
+		if(parsedJSON[i].gameClipUris[p].uriType === 'Download' && !fs.existsSync(pathToFile)){
+			downloadArray.push({
+				'recordedDate':parsedJSON[i].dateRecorded,
+				'recordedDateFriendly':parsedJSON[i].dateRecorded.replace(/\W+/g, "-"),
+				'clipTitle':parsedJSON[i].titleName,
+				'clipTitleFriendly':parsedJSON[i].titleName.replace(/\W+/g, ""),
+				'clipDownloadURL':parsedJSON[i].gameClipUris[p].uri,
+				'clipDownloadDir':fileDir,
+				'clipDownloadAbsolutePath':pathToFile
+			});
 		}
 	};
 };
+
+var getSingleClipDefer = Q.defer();
+var currentClip = 0;
+var promisesPromises = [];
+
+
+var downloadFile = function(callback) {
+    // if(currentClip <= downloadArray.length) {
+    if(currentClip <= 2) {
+
+        var download = downloadArray[currentClip].clipDownloadURL;
+        var file = fs.createWriteStream(downloadArray[currentClip].clipDownloadAbsolutePath);
+
+        http.get(download, function(response) {
+            
+        	// pipe the response to the file
+			response.pipe(file);
+
+			file.on('error', function(error){
+				// console.log('Something went wrong trying to download the clip from this URL: ' + downloadArray[currentClip].clipDownloadURL);
+				// console.log('Does the directory: ' + downloadArray[currentClip].clipDownloadDir + ' have read / write access allowed for node?');
+				console.log('The specific error seen was:');
+				console.log(error);
+			});
+
+			file.on('finish', function() {
+		      file.close(function(){
+		      	console.log('Finished processing.');
+		      });
+		    });
+
+            currentClip++;
+            downloadFile(callback);
+        });
+    } else {
+        callback();
+    }
+}
+
+downloadFile(function() {
+    console.log("Download for "
+    	+ gamertag
+    	+ '-'
+    	+ downloadArray[currentClip].clipTitleFriendly
+    	+ '-'
+    	+ downloadArray[currentClip].recordedDateFriendly
+    	+ " complete.");
+});
+
+
+
+
+
+
+
+
+
+
+// var advanceToNextClip = function(dataArray,callback){
+// 	if(currentClip <= dataArray.length){
+
+// 		cp.exec('node xboxOneGameClipDownloader.js '
+// 			+ '"'
+// 			+ dataArray[currentClip].clipTitleFriendly
+// 			+ '" "'
+// 			+ dataArray[currentClip].recordedDateFriendly
+// 			+ '" "'
+// 			+ dataArray[currentClip].clipDownloadDir
+// 			+ '" "'
+// 			+ dataArray[currentClip].clipDownloadURL
+// 			+ '" "'
+// 			+ gamertag
+// 			+ '"',
+// 			function(error,stdout,stderr){
+// 				if(error){
+// 					console.log('error encountered');
+// 					console.log(stdout);
+// 					console.log(stderr);
+// 					getSingleClipDefer.reject(error);
+// 				} else {
+// 					console.log(stdout);
+// 					console.log('Clip downloaded to: ' + dataArray[currentClip].clipDownloadAbsolutePath);
+// 					getSingleClipDefer.resolve(stdout);
+// 				}
+// 				currentClip++;
+// 			});
+
+// 		return getSingleClipDefer.promise.then(function(result){
+// 			return callback(dataArray,callback);
+// 		});
+
+// 	} else {
+// 		// all done
+// 		return;
+// 	}
+// }
+
+
+// var advanceToNextClipCallback = function(dataArray,callback){
+// 	if(currentClip <= dataArray.length){
+
+// 		cp.exec('node xboxOneGameClipDownloader.js '
+// 			+ '"'
+// 			+ dataArray[currentClip].clipTitleFriendly
+// 			+ '" "'
+// 			+ dataArray[currentClip].recordedDateFriendly
+// 			+ '" "'
+// 			+ dataArray[currentClip].clipDownloadDir
+// 			+ '" "'
+// 			+ dataArray[currentClip].clipDownloadURL
+// 			+ '" "'
+// 			+ gamertag
+// 			+ '"',
+// 			function(error,stdout,stderr){
+// 				if(error){
+// 					console.log('error encountered');
+// 					console.log(stdout);
+// 					console.log(stderr);
+// 					getSingleClipDefer.reject(error);
+// 				} else {
+// 					console.log(stdout);
+// 					console.log('Clip downloaded to: ' + dataArray[currentClip].clipDownloadAbsolutePath);
+// 					getSingleClipDefer.resolve(stdout);
+// 				}
+// 				currentClip++;
+// 			});
+
+// 		return getSingleClipDefer.promise.then(function(result){
+// 			return callback(dataArray,callback);
+// 		});
+		
+// 	} else {
+// 		// all done
+// 		return;
+// 	}
+// }
+
+
+
+
+
+// cp.exec('node xboxOneGameClipDownloader.js '
+// 		+ '"'
+// 		+ downloadArray[currentClip].clipTitleFriendly
+// 		+ '" "'
+// 		+ downloadArray[currentClip].recordedDateFriendly
+// 		+ '" "'
+// 		+ downloadArray[currentClip].clipDownloadDir
+// 		+ '" "'
+// 		+ downloadArray[currentClip].clipDownloadURL
+// 		+ '" "'
+// 		+ gamertag
+// 		+ '"',
+// 		function(error,stdout,stderr){
+// 			if(error){
+// 				console.log('error encountered');
+// 				console.log(stdout);
+// 				console.log(stderr);
+// 				getSingleClipDefer.reject(error);
+// 			} else {
+// 				console.log('- - - - - - -');
+// 				console.log(stdout);
+// 				console.log('Clip downloaded to: ' + downloadArray[currentClip].clipDownloadAbsolutePath + '.MP4');
+// 				getSingleClipDefer.resolve(stdout);
+// 			}
+// 			currentClip++;
+// 		}
+// 	);
+
+// return getSingleClipDefer.promise.then(function(result){
+
+
+// 	// console.log(result);
+// 	// return advanceToNextClip(downloadArray,advanceToNextClipCallback);
+// });
+
--- xboxOneClips.js
+++ xboxOneClips.js
@@ -8,6 +8,10 @@ var parser,
 	xboxOneGamertag,
 	downloadDirectory = null;
 var verbose = '';
+var resPromise1,resPromise2,resPromise3,resPromise4,resPromise5,resPromise6,resPromise7,resPromise8,resPromise9,resPromise10,
+	resPromise11,resPromise12,resPromise13,resPromise14,resPromise15,resPromise16,resPromise17,resPromise18,resPromise19,resPromise20,
+	resPromise21,resPromise22,resPromise23,resPromise24,resPromise25,resPromise26,resPromise27,resPromise28,resPromise29,resPromise30 = Q.defer();
+
 
 var kHelptext = "-k must be a string that matches your xboxapi.com apikey";
 var tHelptext = "-t must be a string that matches a valid Xbox One gamertag";
@@ -46,9 +50,408 @@ cp.exec('node getXboxOneGameClips.js "' + xboxOneApiKey + '" "' + downloadDirect
 	}
 });
 
-return getClipsDeferred.promise.then(function(result){
-	//console.log(result);
-	var json = require(fileDirectory + '/' + xboxOneGamertag + 'Clips');
-	console.log(json);
-	console.log('EXECUTED');
-});
+var clipIndex = 0;
+var stopAll = false;
+
+
+var recursiveCallback = function(data, callback){
+	if(stopAll){
+		// we're all done
+		return;
+	} else {
+		return Q.defer().then(callback);
+	}
+};
+
+return getClipsDeferred
+	.promise
+	.then(function(result){
+		var resPromise1 = Q.defer();
+		console.log('starting promise chain step 1');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise1.resolve(true);
+			});
+		return resPromise1.promise;
+	})
+	.then(function(result){
+		var resPromise2 = Q.defer();
+		console.log('starting promise chain step 2');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise2.resolve(true);
+			});
+		return resPromise2.promise;
+	})
+	.then(function(result){
+		var resPromise3 = Q.defer();
+		console.log('starting promise chain step 3');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise3.resolve(true);
+			});
+		return resPromise3.promise;
+	})
+	.then(function(result){
+		var resPromise4 = Q.defer();
+		console.log('starting promise chain step 4');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise4.resolve(true);
+			});
+		return resPromise4.promise;
+	})
+	.then(function(result){
+		var resPromise5 = Q.defer();
+		console.log('starting promise chain step 5');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise5.resolve(true);
+			});
+		return resPromise5.promise;
+	})
+	.then(function(result){
+		var resPromise6 = Q.defer();
+		console.log('starting promise chain step 6');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise6.resolve(true);
+			});
+		return resPromise6.promise;
+	})
+	.then(function(result){
+		var resPromise7 = Q.defer();
+		console.log('starting promise chain step 7');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise7.resolve(true);
+			});
+		return resPromise7.promise;
+	})
+	.then(function(result){
+		var resPromise8 = Q.defer();
+		console.log('starting promise chain step 8');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise8.resolve(true);
+			});
+		return resPromise8.promise;
+	})
+	.then(function(result){
+		var resPromise9 = Q.defer();
+		console.log('starting promise chain step 9');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise9.resolve(true);
+			});
+		return resPromise9.promise;
+	})
+	.then(function(result){
+		var resPromise10 = Q.defer();
+		console.log('starting promise chain step 10');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise10.resolve(true);
+			});
+		return resPromise10.promise;
+	})
+	.then(function(result){
+		var resPromise11 = Q.defer();
+		console.log('starting promise chain step 11');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise11.resolve(true);
+			});
+		return resPromise11.promise;
+	})
+	.then(function(result){
+		var resPromise12 = Q.defer();
+		console.log('starting promise chain step 12');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise12.resolve(true);
+			});
+		return resPromise12.promise;
+	})
+	.then(function(result){
+		var resPromise13 = Q.defer();
+		console.log('starting promise chain step 13');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise13.resolve(true);
+			});
+		return resPromise13.promise;
+	})
+	.then(function(result){
+		var resPromise14 = Q.defer();
+		console.log('starting promise chain step 14');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise14.resolve(true);
+			});
+		return resPromise14.promise;
+	})
+	.then(function(result){
+		var resPromise15 = Q.defer();
+		console.log('starting promise chain step 15');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise15.resolve(true);
+			});
+		return resPromise15.promise;
+	})
+	.then(function(result){
+		var resPromise16 = Q.defer();
+		console.log('starting promise chain step 16');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise16.resolve(true);
+			});
+		return resPromise16.promise;
+	})
+	.then(function(result){
+		var resPromise17 = Q.defer();
+		console.log('starting promise chain step 17');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise17.resolve(true);
+			});
+		return resPromise17.promise;
+	})
+	.then(function(result){
+		var resPromise18 = Q.defer();
+		console.log('starting promise chain step 18');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise18.resolve(true);
+			});
+		return resPromise18.promise;
+	})
+	.then(function(result){
+		var resPromise19 = Q.defer();
+		console.log('starting promise chain step 19');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise19.resolve(true);
+			});
+		return resPromise19.promise;
+	})
+	.then(function(result){
+		var resPromise10 = Q.defer();
+		console.log('starting promise chain step 20');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise20.resolve(true);
+			});
+		return resPromise20.promise;
+	})
+	.then(function(result){
+		var resPromise21 = Q.defer();
+		console.log('starting promise chain step 21');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise21.resolve(true);
+			});
+		return resPromise21.promise;
+	})
+	.then(function(result){
+		var resPromise22 = Q.defer();
+		console.log('starting promise chain step 22');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise22.resolve(true);
+			});
+		return resPromise22.promise;
+	})
+	.then(function(result){
+		var resPromise23 = Q.defer();
+		console.log('starting promise chain step 23');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise23.resolve(true);
+			});
+		return resPromise23.promise;
+	})
+	.then(function(result){
+		var resPromise24 = Q.defer();
+		console.log('starting promise chain step 24');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise24.resolve(true);
+			});
+		return resPromise24.promise;
+	})
+	.then(function(result){
+		var resPromise25 = Q.defer();
+		console.log('starting promise chain step 25');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise25.resolve(true);
+			});
+		return resPromise25.promise;
+	})
+	.then(function(result){
+		var resPromise26 = Q.defer();
+		console.log('starting promise chain step 26');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise26.resolve(true);
+			});
+		return resPromise26.promise;
+	})
+	.then(function(result){
+		var resPromise27 = Q.defer();
+		console.log('starting promise chain step 27');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise27.resolve(true);
+			});
+		return resPromise27.promise;
+	})
+	.then(function(result){
+		var resPromise28 = Q.defer();
+		console.log('starting promise chain step 28');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise28.resolve(true);
+			});
+		return resPromise28.promise;
+	})
+	.then(function(result){
+		var resPromise29 = Q.defer();
+		console.log('starting promise chain step 29');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise29.resolve(true);
+			});
+		return resPromise29.promise;
+	})
+	.then(function(result){
+		var resPromise30 = Q.defer();
+		console.log('starting promise chain step 30');
+		cp.exec('node parseXboxOneGameClipsResponse.js "'
+			+ downloadDirectory + '/' + xboxOneGamertag + 'Clips" "'
+			+ downloadDirectory + '" "'
+			+ xboxOneGamertag + '"',
+			function(error,stdout,stderr){
+				console.log(stdout);
+				resPromise30.resolve(true);
+			});
+		return resPromise30.promise;
+	});
