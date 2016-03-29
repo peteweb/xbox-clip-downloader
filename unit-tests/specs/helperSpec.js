51c04a1d24d2ad28c6d10424688bbc1590dfbd56
Peter Lancaster <petedoeswebthings@gmail.com>
2016-03-29 01:04:49 +0100

    Added in initial unit test for helper.js - not finished - more to follow in the future.

--- unit-tests/specs/helperSpec.js
+++ unit-tests/specs/helperSpec.js
@@ -1,9 +1,21 @@
+var proxyquire =  require('proxyquire');
+
 describe("helper.js works as expected", function() {
 
+    var testing = proxyquire('../../helper.js', {});
 
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+    it("can determine bad values through isNotNullUndefinedEmpty", function() {
+        expect(testing.isNotNullUndefinedEmpty("")).toBe(false);
+        expect(testing.isNotNullUndefinedEmpty(null)).toBe(false);
+        expect(testing.isNotNullUndefinedEmpty(undefined)).toBe(false);
     });
 
-    
+    it("can determine good values through isNotNullUndefinedEmpty", function() {
+        expect(testing.isNotNullUndefinedEmpty("a")).toBe(true);
+        expect(testing.isNotNullUndefinedEmpty(1)).toBe(true);
+        expect(testing.isNotNullUndefinedEmpty(["hello"])).toBe(true);
+        expect(testing.isNotNullUndefinedEmpty({hello:'there'})).toBe(true);
+    });
+
+
 });
